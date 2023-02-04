4a78a23bdaae471a19f388c5f5acab6c61c0c115
OpaChaUA <opaCha.ua.94@gmail.com>
2023-02-04 19:37:12 +0200

    error correction and deleting blanks.

--- src/modul9/MyArrayList.java
+++ src/modul9/MyArrayList.java
@@ -1,42 +1,40 @@
 package modul9;
+
 import java.util.Arrays;
 import java.util.Objects;
 
-class MyArrayList<T> implements List<T>{
+class MyArrayList<T> implements List<T> {
 
-    public static final int DEFAULT_CAPACITY =5;
-    private Object[]elements;
+    public static final int DEFAULT_CAPACITY = 5;
+    private Object[] elements;
 
     private int size;
-    public MyArrayList(){
-        elements=new Object[DEFAULT_CAPACITY];
-        size=0;
+
+    public MyArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+        size = 0;
 
     }
 
     @Override
     public void add(T element) {
-        if (size<=elements.length){
-            elements[size++]=element;
+        if (size <= elements.length) {
+            elements[size++] = element;
         }
-        if(size>=elements.length) {
+        if (size >= elements.length) {
             Object[] newArray = new Object[elements.length * 2];
-            System.arraycopy(elements, 0, newArray,0,size);
-            elements=newArray;
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = newArray;
         }
-
-
-        }
-
+    }
 
 
     @Override
-    @SuppressWarnings("unchecked")
     public T remove(int index) {
         T remElements = (T) elements[index];
         Objects.checkIndex(index, size);
-        System.arraycopy(elements, index+1, elements, index, size - index -1);
-       size--;
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
         return remElements;
     }
 
@@ -48,15 +46,14 @@ class MyArrayList<T> implements List<T>{
 
     @Override
     public void clear() {
-        elements=new Object[DEFAULT_CAPACITY];
-        size=0;
+        elements = new Object[DEFAULT_CAPACITY];
+        size = 0;
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public T get(int index) {
         Objects.checkIndex(index, size);
-        return (T)elements[index];
+        return (T) elements[index];
     }
-    }
+}
 
